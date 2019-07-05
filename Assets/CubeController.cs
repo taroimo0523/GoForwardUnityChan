519b4e6ebc391160fac64907a32b3d0d37009cff
taroimo0523 <taroimoaki@gmail.com>
2019-07-05 15:43:25 +0900

    課題提出
    void OnCollisionEnter2Dを使って、キューブと地面に衝突したときに音が鳴るようにしました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //衝突した時に音をつける
+    void OnCollisionEnter2D(Collision2D collision) {
+
+        //cubeprefabにぶつかったときに音を鳴らす
+        if (collision.gameObject.tag == "CubePrefab")
+        {
+            GetComponent<AudioSource>().Play();
+            
+        }
+        //地面にぶつかったときに音を鳴らす
+        if (collision.gameObject.name == "ground")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
